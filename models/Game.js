96ea52eb0f2b217e33dba6e96d400335f6d29228
KienDuc24 <92088660+KienDuc24@users.noreply.github.com>
2025-11-13 02:51:42 +0700

    update model

--- models/Game.js
+++ models/Game.js
@@ -17,7 +17,11 @@ const gameSchema = new Schema({
   name: { type: LangSchema, required: true },
   desc: { type: LangSchema, required: true },
   players: { type: String, required: true },
-  category: { type: LangSchema, required: true }
+  category: { type: LangSchema, required: true },
+  featured: {
+    type: Boolean,
+    default: false, // Mặc định là không nổi bật
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Game', gameSchema);
