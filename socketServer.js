3689999d360b4c80741c96c205f6f66f99261fb4
KienDuc24 <92088660+KienDuc24@users.noreply.github.com>
2025-12-02 01:36:15 +0700

    Comment out tic-tac-toe socket handler import

--- socketServer.js
+++ socketServer.js
@@ -4,7 +4,7 @@ const User = require('./models/User');
 
 const todHandler = require('./public/game/ToD/todSocket.js'); 
 const drawGuessHandler = require('./public/game/Draw/drawSocket.js'); 
-const ticTacToeHandler = require('./public/game/TicTacToe/ticTacToeSocket.js');
+// const ticTacToeHandler = require('./public/game/TicTacToe/ticTacToeSocket.js');
 const triviaHandler = require('./public/game/Trivia/triviaSocket.js');
 
 const socketUserMap = new Map();
